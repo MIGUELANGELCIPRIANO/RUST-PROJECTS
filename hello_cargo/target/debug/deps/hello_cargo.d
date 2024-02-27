4c7d769a9d9b075e77764624925ffdd55581999b
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-02-27 14:39:20 -0300

    TEST: Compile and run project with Cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\cipri_9p901s4\Desktop\RUST-PROJECTS\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\cipri_9p901s4\Desktop\RUST-PROJECTS\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\cipri_9p901s4\Desktop\RUST-PROJECTS\hello_cargo\target\debug\hello_cargo.exe: C:\Users\cipri_9p901s4\Desktop\RUST-PROJECTS\hello_cargo\src\main.rs
